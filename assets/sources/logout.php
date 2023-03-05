c6f45ef3d8c90d04bf1b293a9e59cce675baa9ea
1MuneebUrRehman <1muneeburrehman@gmail.com>
2023-03-05 14:33:35 +0500

    -Logout is Mobile Added

--- assets/sources/logout.php
+++ assets/sources/logout.php
@@ -7,4 +7,11 @@ if (isset($_SESSION['new_user'])) {
     unset($_SESSION['new_user']);
 }
 $domain = $_SERVER["SERVER_NAME"];
-header('Location: http://'.$domain);
+
+// Is Mobile
+if (preg_match("/(android|blackberry|iphone|ipod|palm|windows\s+ce)/i", $_SERVER["HTTP_USER_AGENT"])) {
+    header('Location: http://'.$domain.'/sbx/mobile');
+}
+else{
+    header('Location: http://'.$domain.'/sbx');
+}
